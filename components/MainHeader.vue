6847f430efef0a6a28d029ad7104c33f8968a171
kylem2002 <146904543+kylem2002@users.noreply.github.com>
2023-12-06 15:17:05 -0500

    Create MainHeader.vue

---
+++ components/MainHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="app-header">
+    <div id="app"></div>
+    <h1>Environmentally Friendly Product Guide</h1>
+
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+}
+</script>
+
+<style scoped>
+  .app-header {
+    width: 100%;
+    background-color: green;
+    color: black;
+    padding: 10px 20px;
+    text-align: center;
+  }
+</style>
