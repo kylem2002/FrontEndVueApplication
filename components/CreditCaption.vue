c703e024c5fcb8fa7cf84248c0669e4bf90ce804
kylem2002 <146904543+kylem2002@users.noreply.github.com>
2023-12-06 15:16:36 -0500

    Create CreditCaption.vue

---
+++ components/CreditCaption.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="app-footer">
+      <h2>Credit for the information goes to: https://kylewebsite.onrender.com/api </h2>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CreditCaption',
+  }
+  </script>
+  
+  <style scoped>
+    .app-footer {
+      width: 100%;
+      background-color: orange;
+      color: black;
+      padding: 10px 20px;
+      text-align: center;
+    }
+  </style>
+  
