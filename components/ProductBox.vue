62e4e0dc86ca523e5f1ffc57cb7e51a92ba5dbb0
kylem2002 <146904543+kylem2002@users.noreply.github.com>
2023-12-06 15:17:29 -0500

    Create ProductBox.vue

---
+++ components/ProductBox.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-for="product in products" :key="product.productNames">
+        {{ product.productNames }}, {{ product.ecoRating }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductBox',
+    props: {
+        products: Array
+    }
+}
+</script>
+
+<style scoped>
+</style>
